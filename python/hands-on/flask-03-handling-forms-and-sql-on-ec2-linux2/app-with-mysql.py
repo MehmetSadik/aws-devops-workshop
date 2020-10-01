1cc39e728b4d27100ca6e099fa41f91fb1e46a35
Mehmet Sadik <strustas@gmail.com>
2020-10-01 22:42:11 +0200

    Portfolio Building S6-Project 001-Roman Numerals Converter- Serdar - 30/09/2020

--- python/hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-mysql.py
+++ python/hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-mysql.py
@@ -1,14 +1,14 @@
 # Import Flask modules
-from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, render_template, request
 from flaskext.mysql import MySQL
 
 # Create an object named app
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+app.config['MYSQL_DATABASE_HOST'] = 'call-mysql-db-server.cbanmzptkrzf.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = '123456789'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql = MySQL()
@@ -27,13 +27,13 @@ CREATE TABLE users (
   email varchar(50),
   PRIMARY KEY (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-"""  # Bu kullanım aslında sqlite ile aynı şeyi ifade etmekle birlikte bazı mysql convention'dan dolayı küçük farklılıklar ouşmaktadır. Bunlardan biri varchar olarak 50 dememiz gerekiyor burada. Ayrıca en sonda ENGINE tipi belirtmemiz gerekmektedir.
+"""
 data = """
 INSERT INTO clarusway.users 
 VALUES 
-    ("Levent Akyuz", "levent.akyuz@gmail.com"),
-    ("Mustafa Kanat", "mustafa.kanat@yahoo.com"),
-	("Hakan Sule", "hakan.sule@clarusway.com");
+    ("Buddy Rich", "buddy@clarusway.com" ),
+    ("Candido", "candido@clarusway.com"),
+	("Charlie Byrd", "charlie.byrd@clarusway.com");
 """
 cursor.execute(drop_table)
 cursor.execute(users_table)
@@ -47,7 +47,7 @@ def find_emails(keyword):
     query = f"""
     SELECT * FROM users WHERE username like '%{keyword}%';
     """
-    cursor.execute(query)  
+    cursor.execute(query)
     result = cursor.fetchall()
     user_emails = [(row[0], row[1]) for row in result]
     # if there is no user with given name in the db, then give warning
@@ -101,11 +101,11 @@ def add_email():
         user_name = request.form['username']
         user_email = request.form['useremail']
         result = insert_email(user_name, user_email)
-        return render_template('add-email.html', result_html=result, show_result=True)
+        return render_template('add-email.html', result=result, show_result=True)
     else:
         return render_template('add-email.html', show_result=False)
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-    app.run(debug=True)
-   #app.run(host='0.0.0.0', port=80)
+#    app.run(debug=True)
+   app.run(host='0.0.0.0', port=80)
